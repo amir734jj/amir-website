d761daf0f3ae740cfa271dbe9d0377a50ea2de5f
Amir Hesamian <hesamian@uwm.edu>
2026-06-14 15:02:57 -0500

    Add optional thesis URL to education entries

--- RazorBlogGenerator/Models/CV/EducationData.cs
+++ RazorBlogGenerator/Models/CV/EducationData.cs
@@ -6,5 +6,6 @@ public class EducationData
     public string Location { get; set; } = null!;
     public string Degree { get; set; } = null!;
     public string Period { get; set; } = null!;
+    public string? ThesisUrl { get; set; }
     public List<string> Details { get; set; } = [];
 }
